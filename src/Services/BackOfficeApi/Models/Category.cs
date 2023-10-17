b5310742b4f79c894fd7f84c66578576fdcdbb65
Diego Browen Proença de Oliveira <diegop.o@hotmail.com>
2023-10-17 20:36:54 -0300

    Create product and category controller

---
+++ src/Services/BackOfficeApi/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿namespace BackOffice.Api.Models;
+
+public class Category
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    public Category()
+    {
+    }
+}
--- src/Services/BackOfficeApi/Program.cs
+++ src/Services/BackOfficeApi/Program.cs
@@ -1,12 +1,26 @@
+using BackOffice.Api.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 
+builder.Services.AddDbContext<BackOfficeContext>(
+    options => options.UseSqlServer("name=ConnectionStrings:DefaultConnection"));
+
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseAuthorization();
 
